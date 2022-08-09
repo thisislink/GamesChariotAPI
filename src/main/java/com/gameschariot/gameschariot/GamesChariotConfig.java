dde20f30850e97ea85df22aa6bfabe63ab33c10b
daphnelink@gmail.com <daphnelink@gmail.com>
2022-08-08 20:53:18 -0700

    Update GamesChariotConfig.java

--- src/main/java/com/gameschariot/gameschariot/GamesChariotConfig.java
+++ src/main/java/com/gameschariot/gameschariot/GamesChariotConfig.java
@@ -3,6 +3,4 @@ package com.gameschariot.gameschariot;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("gameschariot")
-public record GamesChariotConfig(String apiUrl, String databaseServer, String databaseUsername, String databasePassword) {
-
-}
+public record GamesChariotConfig(String apiUrl, String databaseServer, String databaseUsername, String databasePassword) {}
