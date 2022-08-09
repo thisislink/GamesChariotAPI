6ab75dd5e2bc0706956c3f65c8ece0a6569b4750
daphnelink@gmail.com <daphnelink@gmail.com>
2022-08-08 20:02:38 -0700

    Update GamesChariot.java
    remove spacing to trigger changes

--- src/main/java/com/gameschariot/gameschariot/GamesChariot.java
+++ src/main/java/com/gameschariot/gameschariot/GamesChariot.java
@@ -2,11 +2,9 @@ package com.gameschariot.gameschariot;
 
 public class GamesChariot {
     private final String games;
-
     public GamesChariot(String allGames) {
         this.games = allGames;
     }
-
     public String getContent() {
         return games;
     }
