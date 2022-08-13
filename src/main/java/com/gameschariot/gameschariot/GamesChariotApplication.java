d8b3bacc88f8d276e0096dcfb630c2e6b630d352
daphnelink@gmail.com <daphnelink@gmail.com>
2022-08-12 18:10:21 -0700

    Update GamesChariotApplication.java
    added cors

--- src/main/java/com/gameschariot/gameschariot/GamesChariotApplication.java
+++ src/main/java/com/gameschariot/gameschariot/GamesChariotApplication.java
@@ -3,13 +3,30 @@ package com.gameschariot.gameschariot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GamesChariotConfig.class)
 public class GamesChariotApplication {
-
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://boot-hheroku.herokuapp.com"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+				"Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(GamesChariotApplication.class, args);
 	}
